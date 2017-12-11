667261d9176d37a051a4fc1b3d87c1d20823abb9
Maxim Fridental <m.fridental@immowelt.de>
2017-12-11 14:36:48 +0100

    Working prototype

--- dbms/src/Interpreters/GdprInterpreter.cpp
+++ dbms/src/Interpreters/GdprInterpreter.cpp
@@ -41,6 +41,28 @@ GdprInterpreter::~GdprInterpreter()
 }
 
 
+void fillParts(BlockInputStreamPtr parent, MergeTreeStreamPartMap & parts)
+{
+    MergeTreeBaseBlockInputStream* p = dynamic_cast<MergeTreeBaseBlockInputStream*>(parent.get());
+
+    if (p)
+    {
+        for(auto part : p->getDataParts())
+        {
+            String s = part->getFullPath();
+            if (parts.count(s) == 0)
+            {
+                parts[s] = part;
+            }
+        }
+    }
+
+    for(auto child: parent->getChildren())
+    {
+        fillParts(child, parts);
+    }
+}
+
 void fillStreams(BlockInputStreamPtr parent, MergeTreeStreams & streams, MergeTreeStreamPartMap & parts)
 {
     MergeTreeBaseBlockInputStream* p = dynamic_cast<MergeTreeBaseBlockInputStream*>(parent.get());
@@ -66,13 +88,17 @@ void fillStreams(BlockInputStreamPtr parent, MergeTreeStreams & streams, MergeTr
     }
 }
 
+
+
 BlockIO GdprInterpreter::execute()
 {
     auto storage = context.getTable(context.getCurrentDatabase(), table);
     auto merge_tree = dynamic_cast<StorageMergeTree *>(storage.get());
 
-    std::cout << "Gathering all affected parts\n";
+    std::cout << "Determining all affected parts\n";
 
+    // Its not possible to directly ask a MergeTree to return all parts that could match a specific prewhere statement.
+    // So we construct a dummy select statement, and then retrieve the affected parts from the returned input streams.
     String dummy_select = "select * from " + table + " prewhere " + prewhere;
 
     ParserQuery parser(dummy_select.data());
@@ -81,10 +107,22 @@ BlockIO GdprInterpreter::execute()
     auto isq = InterpreterSelectQuery(ast, context, QueryProcessingStage::Complete);
     auto block = isq.execute();
 
-    MergeTreeStreams streams;
     MergeTreeStreamPartMap parts;
-    fillStreams(block.in, streams, parts);
+    fillParts(block.in, parts);
 
+    // Now create input streams that would read the whole parts. We want to read whole parts, because we must replace the whole
+    // affected part with its copy with some modified values. We also want to read all columns, because the column to modify might be
+    // the part of the primary key, so when it gets modified the sorting order will change and all rows must be written anew.
+    MergeTreeStreams streams;
+    for (auto part_pair : parts)
+    {
+        streams.push_back(std::move(std::make_shared<MergeTreeBlockInputStream>(
+                part_pair.second->storage, part_pair.second, 10000000, 0, 0, part_pair.second->storage.getColumnNamesList(),
+                MarkRanges(1, MarkRange(0, part_pair.second->marks_count)),
+                  false, nullptr, "", true, 0, DBMS_DEFAULT_BUFFER_SIZE, false)));
+    }
+
+    // Display affected parts to check how many of them are affected, to be able to track performance problems.
     for (auto pp: merge_tree->getData().getDataParts())
     {
         if (parts.count(pp->getFullPath()) == 1)
@@ -98,6 +136,7 @@ BlockIO GdprInterpreter::execute()
         std::cout << pp->getFullPath() << "\n";
     }
 
+    // Now go through all streams, read them, update the value in memory and write out into a new part
     std::cout << "Searching for occurences\n";
     size_t replaced = 0;
     MergeTreeBlockOutputStream outstream(*merge_tree);
@@ -106,15 +145,21 @@ BlockIO GdprInterpreter::execute()
         auto mypart = stream->getDataParts()[0];
         std::cout << "Scanning " << mypart->getFullPath() << "\n";
         std::vector<Block> blocks;
+        int rows = 0;
+
+        // Read all blocks from the current part
         while(true)
         {
             Block b = stream->read();
             if (b.rows() == 0)
                 break;
 
+            rows += b.rows();
             blocks.push_back(std::move(b));
         }
 
+
+        // For each block, search and replace the values in memory. Here can be some memory leaks.
         size_t found = 0;
         for(auto & b : blocks)
         {
@@ -124,6 +169,8 @@ BlockIO GdprInterpreter::execute()
             ColumnString * col = dynamic_cast<ColumnString *>(oldcolumn.column.get());
             const char* newval = newvalue.c_str();
             size_t newvallen = strlen(newval) + 1;
+
+            // Iterate through all rows of a block. It seems to be compilicated to vectorize this operation.
             for(size_t row = 0; row < b.rows(); ++row)
             {
                 StringRef s = col->getDataAtWithTerminatingZero(row);
@@ -141,6 +188,7 @@ BlockIO GdprInterpreter::execute()
                 }
             }
 
+            //if at least one replacement found, remove the old column and add the new column to the block.
             if (found > 0)
             {
                 b.erase(pos);
@@ -148,19 +196,33 @@ BlockIO GdprInterpreter::execute()
             }
         }
 
+        std::cout << "Scanned " << rows << " rows\n";
+
         if (found > 0)
         {
             std::cout << found << " occurences found\n";
+
+            // As soon as the first occurence found in the current part, we can already detach it, because we know
+            // that we are going to replace it. If we add a new part first, something could happen that would
+            // merge the new and the old parts together, making further deduplication complicated.
+            // OTOH if we detach the part first and then crash failing to write a new part, it would be
+            // possible to recover by attaching the old part again.
             merge_tree->getData().renameAndDetachPart(mypart, "", false, true);
             std::cout << "Storing new part.\n";
             for(auto & b : blocks)
             {
                 outstream.write(b);
             }
+            std::cout << "New part stored.\n"; // unfortunately outstream doesn't return the name of the new part(s), so we can't log them
         }
     }
 
-    std::cout << "Replaced " << replaced << " occurences.\n";
+    std::cout << "Replaced in total " << replaced << " occurences.\n";
+
+    std::cout << "Optimizing " << table << ".\n";
+    merge_tree->optimize(nullptr, nullptr, true, false, context);
+
+    std::cout << "The detached parts can now be deleted.\n";
 
     return {};
 }
--- gdpr.sh
+++ gdpr.sh
@@ -3,6 +3,6 @@ build/dbms/src/Server/clickhouse --local --config=gdprconfig.xml --query="select
 
 build/dbms/src/Server/clickhouse --gdpr --config=gdprconfig.xml --table=testdata --prewhere="someid=2" --column=someprivacyvalue --newvalue=WIDERRUF --oldvalue=2@gmail.com
 
-build/dbms/src/Server/clickhouse --local --config=gdprconfig.xml --query="select * from default.testdata where someid=2"
+build/dbms/src/Server/clickhouse --local --config=gdprconfig.xml --query="select * from default.testdata where someprivacyvalue='WIDERRUF'"
 build/dbms/src/Server/clickhouse --local --config=gdprconfig.xml --query="select count() from default.testdata "
 ls -l /var/lib/clickhouse/data/default/testdata/
