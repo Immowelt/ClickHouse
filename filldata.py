eb0b47d1646cc364a3b8e575676425a9c9cdf4fd
Maxim Fridental <m.fridental@immowelt.de>
2018-01-08 10:12:49 +0100

    .

--- filldata.py
+++ filldata.py
@@ -3,7 +3,7 @@ f = open('data-gdpr', 'w')
 #f.write("insert into default.testdata values\n")
 
 sep = ''
-for id in xrange(0, 1000000):
+for id in xrange(0, 100000000):
 	for d in xrange(10,20):
 		datum = '2017-12-%d' if id > 700000 else '2017-10-%d' if id > 500000 else '2017-11-%d'		
 		f.write("%s('%s', %d, '%d@gmail.com')\n" % (sep, datum % d, id, id))
